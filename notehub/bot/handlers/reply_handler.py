de51af40d7d508a54eb098be386c93c44765697b
ykhdr <yanhudorozhkov@gmail.com>
2023-11-15 15:17:04 +0700

    Removed commented code

--- notehub/bot/handlers/reply_handler.py
+++ notehub/bot/handlers/reply_handler.py
@@ -21,56 +21,6 @@ class ReplyHandler(Handler):
         self.__storage_msg_collector = storage_msg_collector
 
     def setup_handler(self):
-        # @self.__bot.inline_handler(func=lambda query: len(query.query) > 0)
-        # def query_text(query):
-        #     digits_pattern = re.compile(r'^[0-9]+ [0-9]+$', re.MULTILINE)
-        #     try:
-        #         matches = re.match(digits_pattern, query.query)
-        #     except AttributeError as ex:
-        #         return
-        #     num1, num2 = matches.group().split()
-        #     try:
-        #         m_sum = int(num1) + int(num2)
-        #         r_sum = InlineQueryResultArticle(
-        #             id='1', title="Сумма",
-        #             # Описание отображается в подсказке,
-        #             # message_text - то, что будет отправлено в виде сообщения
-        #             description="Результат: {!s}".format(m_sum),
-        #             input_message_content=InputTextMessageContent(
-        #                 message_text="{!s} + {!s} = {!s}".format(num1, num2, m_sum))
-        #         )
-        #         m_sub = int(num1) - int(num2)
-        #         r_sub = InlineQueryResultArticle(
-        #             id='2', title="Разность",
-        #             description="Результат: {!s}".format(m_sub),
-        #             input_message_content=InputTextMessageContent(
-        #                 message_text="{!s} - {!s} = {!s}".format(num1, num2, m_sub))
-        #         )
-        #         # Учтем деление на ноль и подготовим 2 варианта развития событий
-        #         if num2 is not "0":
-        #             m_div = int(num1) / int(num2)
-        #             r_div = InlineQueryResultArticle(
-        #                 id='3', title="Частное",
-        #                 description="Результат: {0:.2f}".format(m_div),
-        #                 input_message_content=InputTextMessageContent(
-        #                     message_text="{0!s} / {1!s} = {2:.2f}".format(num1, num2, m_div))
-        #             )
-        #         else:
-        #             r_div = InlineQueryResultArticle(
-        #                 id='3', title="Частное", description="На ноль делить нельзя!",
-        #                 input_message_content=InputTextMessageContent(
-        #                     message_text="Я нехороший человек и делю на ноль!")
-        #             )
-        #         m_mul = int(num1) * int(num2)
-        #         r_mul = InlineQueryResultArticle(
-        #             id='4', title="Произведение",
-        #             description="Результат: {!s}".format(m_mul),
-        #             input_message_content=InputTextMessageContent(
-        #                 message_text="{!s} * {!s} = {!s}".format(num1, num2, m_mul))
-        #         )
-        #         self.__bot.answer_inline_query(query.id, [r_sum, r_sub, r_div, r_mul])
-        #     except Exception as e:
-        #         print("{!s}\n{!s}".format(type(e), str(e)))
 
         @self.__bot.message_handler(func=lambda message: message.text in BotTypes.get_reply_commands_list())
         def handle_reply_buttons_message(message: Message):
